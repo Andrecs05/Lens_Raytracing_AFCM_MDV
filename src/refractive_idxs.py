a318e1f207ba9fb53c377073ea576dcdbaf6ccb5
Andrecs05 <andresfcontreras@icloud.com>
2026-08-27 13:22:38 -0500

    :rocket: Add microscope system described by J. A. Kurvits  doi: 10.1364/josaa.32.002082

--- src/refractive_idxs.py
+++ src/refractive_idxs.py
@@ -35,6 +35,38 @@ def refractive_index(lambda_um, material):
         return refractive_index_N_LAF21(lambda_um)
     elif material == "N_K5":
         return refractive_index_N_K5(lambda_um)
+    elif material == "S_NSL3":
+        return refractive_index_S_NSL3(lambda_um)
+    elif material == "S_LAH79":
+        return refractive_index_S_LAH79(lambda_um)
+    elif material == "GFK68":
+        return refractive_index_GFK68(lambda_um)
+    elif material == "E_F2":
+        return refractive_index_E_F2(lambda_um)
+    elif material == "GFK70":
+        return refractive_index_GFK70(lambda_um)
+    elif material == "N_KZFS8":
+        return refractive_index_N_KZFS8(lambda_um)
+    elif material == "LITHO_CAF2":
+        return refractive_index_LITHO_CAF2(lambda_um)
+    elif material == "N_KZFS5":
+        return refractive_index_N_KZFS5(lambda_um)
+    elif material == "J_PSK03":
+        return refractive_index_J_PSK03(lambda_um)
+    elif material == "J_LASF015":
+        return refractive_index_J_LASF015(lambda_um)
+    elif material == "S_LAH66":
+        return refractive_index_S_LAH66(lambda_um)
+    elif material == "J_SF03":
+        return refractive_index_J_SF03(lambda_um)
+    elif material == "E_SK10":
+        return refractive_index_E_SK10(lambda_um)
+    elif material == "J_LAF7":
+        return refractive_index_J_LAF7(lambda_um)
+    elif material == "BASF6":
+        return refractive_index_BASF6(lambda_um)
+    elif material == "KZFH1":
+        return refractive_index_KZFH1(lambda_um)
     else:
         raise ValueError("Material not recognized")
 
@@ -371,4 +403,531 @@ def refractive_index_N_K5(lambda_um):
         B3 * lambda_sq / (lambda_sq - C3)
     )
 
+    return np.sqrt(n_squared)
+
+def refractive_index_S_NSL3(lambda_um):
+    """
+    Calculates the refractive index n of the material S-NSL3 for a given wavelength using the Sellmeier equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+    
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    B1 = 8.82514764E-01
+    B2 = 3.89271907E-01
+    B3 = 1.10693448E+00
+
+    C1 = 4.64504582E-03
+    C2 = 2.00551397E-02
+    C3 = 1.36234339E+02
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = 1 + (
+        (B1 * lambda_sq) / (lambda_sq - C1) +
+        (B2 * lambda_sq) / (lambda_sq - C2) +
+        (B3 * lambda_sq) / (lambda_sq - C3)
+    )
+    
+    return np.sqrt(n_squared)
+
+
+
+def refractive_index_S_LAH79(lambda_um):
+    """
+    Calculates the refractive index n of S-LAH79 for a given wavelength using
+    the Sellmeier equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    B1 = 2.32557148E+00
+    B2 = 0.507967133
+    B3 = 2.43087198
+
+    C1 = 0.0132895208
+    C2 = 0.0528335449
+    C3 = 161.122408
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = 1 + (
+        (B1 * lambda_sq) / (lambda_sq - C1) +
+        (B2 * lambda_sq) / (lambda_sq - C2) +
+        (B3 * lambda_sq) / (lambda_sq - C3)
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_GFK68(lambda_um):
+    """
+    Calculates the refractive index n of GFK68 for a given wavelength using
+    the dispersion equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.4994867
+    A1 = -0.0059285872
+    A2 = 0.012743526
+    A3 = 0.00015469636
+    A4 = 3.9947612e-06
+    A5 = -1.7603560e-07
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 * lambda_um ** -2 +
+        A3 * lambda_um ** -4 +
+        A4 * lambda_um ** -6 +
+        A5 * lambda_um ** -8
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_E_F2(lambda_um):
+    """
+    Calculates the refractive index n of E-F2 for a given wavelength using
+    the dispersion equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.55739282
+    A1 = -0.0107951229
+    A2 = 0.0216807328
+    A3 = 0.00105165979
+    A4 = -5.36309816e-05
+    A5 = 7.7466326e-06
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 * lambda_um ** -2 +
+        A3 * lambda_um ** -4 +
+        A4 * lambda_um ** -6 +
+        A5 * lambda_um ** -8
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_GFK70(lambda_um):
+    """
+    Calculates the refractive index n of GFK70 for a given wavelength using
+    the dispersion equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.4293935
+    A1 = -0.0057591097
+    A2 = 0.011512013
+    A3 = 0.0001324924
+    A4 = 7.3250033e-06
+    A5 = -5.7195445e-07
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 * lambda_um ** -2 +
+        A3 * lambda_um ** -4 +
+        A4 * lambda_um ** -6 +
+        A5 * lambda_um ** -8
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_N_KZFS8(lambda_um):
+    """
+    Calculates the refractive index n of N-KZFS8 for a given wavelength using
+    the Sellmeier equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    B1 = 1.62693651
+    B2 = 0.24369876
+    B3 = 1.62007141
+
+    C1 = 0.010880863
+    C2 = 0.0494207753
+    C3 = 131.009163
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = 1 + (
+        (B1 * lambda_sq) / (lambda_sq - C1) +
+        (B2 * lambda_sq) / (lambda_sq - C2) +
+        (B3 * lambda_sq) / (lambda_sq - C3)
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_LITHO_CAF2(lambda_um):
+    """
+    Calculates the refractive index n of LITHO-CAF2 for a given wavelength
+    using the Sellmeier equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    B1 = 0.617617011
+    B2 = 0.421117656
+    B3 = 3.79711183
+
+    C1 = 0.00275381936
+    C2 = 0.0105900875
+    C3 = 1182.67444
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = 1 + (
+        (B1 * lambda_sq) / (lambda_sq - C1) +
+        (B2 * lambda_sq) / (lambda_sq - C2) +
+        (B3 * lambda_sq) / (lambda_sq - C3)
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_N_KZFS5(lambda_um):
+    """
+    Calculates the refractive index n of N-KZFS5 for a given wavelength using
+    the Sellmeier equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    B1 = 1.47460789
+    B2 = 0.193584488
+    B3 = 1.265899740
+
+    C1 = 0.00986143816
+    C2 = 0.0445477583
+    C3 = 106.436258
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = 1 + (
+        (B1 * lambda_sq) / (lambda_sq - C1) +
+        (B2 * lambda_sq) / (lambda_sq - C2) +
+        (B3 * lambda_sq) / (lambda_sq - C3)
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_J_PSK03(lambda_um):
+    """
+    Calculates the refractive index n of J-PSK03 for a given wavelength using
+    the dispersion equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.53267453
+    A1 = -0.00950416844
+    A2 = -0.000106883723
+    A3 = 0.013439736
+    A4 = 0.000141770605
+    A5 = 4.7304388e-06
+    A6 = -8.6200083e-08
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 * lambda_um ** 4 +
+        A3 * lambda_um ** -2 +
+        A4 * lambda_um ** -4 +
+        A5 * lambda_um ** -6 +
+        A6 * lambda_um ** -8
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_J_LASF015(lambda_um):
+    """
+    Calculates the refractive index n of J-LASF015 for a given wavelength
+    using the dispersion equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 3.17452404
+    A1 = -0.0132156517
+    A2 = -0.000165919934
+    A3 = 0.0276472367
+    A4 = 0.000483338934
+    A5 = 1.20380702e-05
+    A6 = 6.02649728e-07
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 * lambda_um ** 4 +
+        A3 * lambda_um ** -2 +
+        A4 * lambda_um ** -4 +
+        A5 * lambda_um ** -6 +
+        A6 * lambda_um ** -8
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_S_LAH66(lambda_um):
+    """
+    Calculates the refractive index n of S-LAH66 for a given wavelength using
+    the Sellmeier equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    B1 = 1.39280586
+    B2 = 0.679577094
+    B3 = 1.38702069
+
+    C1 = 0.00608475118
+    C2 = 0.0233925351
+    C3 = 95.8354094
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = 1 + (
+        (B1 * lambda_sq) / (lambda_sq - C1) +
+        (B2 * lambda_sq) / (lambda_sq - C2) +
+        (B3 * lambda_sq) / (lambda_sq - C3)
+    )
+
+    return np.sqrt(n_squared)
+
+
+def refractive_index_J_SF03(lambda_um):
+    """
+    Calculates the refractive index n of J-SF03 for a given wavelength using
+    the dispersion equation.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 3.25089291
+    A1 = -0.013324411
+    A2 = 0.0484040988
+    A3 = 0.0032638368
+    A4 = -0.000401470701
+    A5 = 0.000116583198
+    A6 = -1.27242455e-05
+    A7 = 6.96171808e-07
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 * lambda_um ** -2 +
+        A3 * lambda_um ** -4 +
+        A4 * lambda_um ** -6 +
+        A5 * lambda_um ** -8 +
+        A6 * lambda_um ** -10 +
+        A7 * lambda_um ** -12
+    )
+
+    return np.sqrt(n_squared)
+
+def refractive_index_E_SK10(lambda_um):
+    """
+    Calculates the refractive index n of the material E-SK10
+    for a given wavelength.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+    
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.58912326
+    A1 = -0.0100115186
+    A2 = 0.0156165805
+    A3 = 0.000328758605
+    A4 = -9.84858579e-06
+    A5 = 7.82877169e-07
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 / lambda_sq +
+        A3 / lambda_sq**2 +
+        A4 / lambda_sq**3 +
+        A5 / lambda_sq**4
+    )
+    
+    return np.sqrt(n_squared)
+
+
+
+def refractive_index_J_LAF7(lambda_um):
+    """
+    Calculates the refractive index n of the material J-LAF7
+    for a given wavelength.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+    
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.96739544
+    A1 = -0.0118139418
+    A2 = -0.000133628078
+    A3 = 0.0310749099
+    A4 = 0.000654571893
+    A5 = 9.85567905e-05
+    A6 = -8.8311254e-06
+    A7 = 8.38843732e-07
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 * lambda_sq**2 +
+        A3 / lambda_sq +
+        A4 / lambda_sq**2 +
+        A5 / lambda_sq**3 +
+        A6 / lambda_sq**4 +
+        A7 / lambda_sq**5
+    )
+    
+    return np.sqrt(n_squared)
+
+
+def refractive_index_BASF6(lambda_um):
+    """
+    Calculates the refractive index n of the material BASF6
+    for a given wavelength.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+    
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.718163
+    A1 = -0.01334622
+    A2 = 0.02057831
+    A3 = 0.0009283221
+    A4 = -1.411159e-05
+    A5 = 1.659476e-06
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 / lambda_sq +
+        A3 / lambda_sq**2 +
+        A4 / lambda_sq**3 +
+        A5 / lambda_sq**4
+    )
+    
+    return np.sqrt(n_squared)
+
+
+def refractive_index_KZFH1(lambda_um):
+    """
+    Calculates the refractive index n of the material KZFH1
+    for a given wavelength.
+
+    Args:
+        lambda_um (float or ndarray): Wavelength in micrometers (µm)
+    
+    Returns:
+        n (float or ndarray): Refractive index
+    """
+
+    A0 = 2.547175
+    A1 = -0.01287115
+    A2 = 0.01838384
+    A3 = 0.0005862006
+    A4 = -1.624561e-05
+    A5 = 2.266107e-06
+
+    lambda_sq = lambda_um ** 2
+
+    n_squared = (
+        A0 +
+        A1 * lambda_sq +
+        A2 / lambda_sq +
+        A3 / lambda_sq**2 +
+        A4 / lambda_sq**3 +
+        A5 / lambda_sq**4
+    )
+    
     return np.sqrt(n_squared)
